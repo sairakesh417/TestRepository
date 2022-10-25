17c5f3311f340b84b532237f6fc9b2582d3fbaff
sairakesh417 <116661067+sairakesh417@users.noreply.github.com>
2022-10-25 21:08:37 +0530

    Update TestClass.java
    Changed the sysout Message

--- TestProject/src/com/TestClass.java
+++ TestProject/src/com/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Welcome to my Test Repository!!");
 		
-		System.out.println("New Branch Added from Rakesh");
+		System.out.println("New Branch is Added from Rakesh");
 	}
 
 }
